eaf3f88befcd7ba0103be7606d5fba26bc9d088e
qihengchen <qiheng_chen@alumni.brown.edu>
2020-05-07 14:05:01 -0400

    added comments

--- analyze_april.py
+++ analyze_april.py
@@ -4,53 +4,62 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from math import ceil, floor
+from sys import argv
 
 
 def group(df, dvmin, dvmax, step):
+	""" dataframe, int, int, int -> list
+	Helper function. Calculate the probability of choosing left for each bucket, and return them in a list.
+	"""
 	r = step/2
 	res = []
 
 	for ticker in range(dvmin, dvmax, step):
+		#select values by left-right  difference in sum in range (x-r, x+r). x is the middle value of a bucket. 
 		subgroup = df.loc[(df['diff']>ticker-r) & (df['diff']<ticker+r)
 			& (df['choice'] != 0.5)]
+		#count frequency of choosing left
 		num = subgroup['choice'].sum()
+		#total number of datapoints in the bucket
 		denom = subgroup.shape[0]
-		"""
-		if denom:
-			print(int(ticker-r), int(ticker+r), int(num), denom, num/denom)
-		else:
-			print('denom 0 \n')
-		"""
+		#calculate and append the prob. append 0 if empty bucket
 		res.append(num/denom) if denom else res.append(0)
 	return res
 
-# stacked_df, left values, right values, 
-def plot_figure3(df, left_colNames, right_colNames, dvmin=-100, dvmax=101, step=20, show=False):
+
+def plot_figure3(df, left_colNames, right_colNames, dvmin=-100, dvmax=101, step=20, show=False, write_to=None):
 	"""
-	dataframe, list, list, int, int, int -> None
+	dataframe, list, list, int, int, int, bool, str -> np.array, np.array
 	"""
+	#select left values and right values
 	left = df[left_colNames]
 	right = df[right_colNames]
+	# 'diff' column: sum(left) - sum(right)
 	lr_diff = pd.DataFrame({'diff': left.sum(axis=1)-right.sum(axis=1)})
+	# 'side_chosen': 0 left, 1 right 
+	# 'choice': 1 chose left, 0 chose right. Flipped 'side_chosen' column, b/c we will count the frequency of choosing left
 	lr_diff['choice'] = np.logical_xor(df['side_chosen'],1).astype(int)
+	# sort diff from lowest to highest, and group into buckets
 	lr_diff = lr_diff.sort_values(by=['diff'], ascending=True)
-
+	#array of prob(choosing left) for each bucket
 	grouped = group(lr_diff, dvmin, dvmax, step)
 
+	#plot data
 	y = np.array(grouped)
 	x = np.array([x for x in range(dvmin, dvmax, step)])
-
 	if show:
 		fig = go.Figure()
 		fig.add_trace(go.Scatter(x=x, y=y, name="linear", line_shape='linear'))
 		fig.show()
-
+	if write_to is not None:
+		fig.write_image(write_to)
 	return x, y
 
 def plot_figure3_subplots(df, left_colNames, right_colNames, dvmin=-100, dvmax=101, step=20, show=False, write_to=None):
 	fig = make_subplots(rows=2, cols=3, subplot_titles=('sim-numbers', 'alt-numbers', 'seq-numbers', 'sim-bars', 'alt-bars', 'seq-bars'))
 	x = np.array([x for x in range(dvmin, dvmax, step)])
 	
+	#call plot_figure3() for each block in the order of 'sim-numbers', 'alt-numbers', 'seq-numbers', 'sim-bars', 'alt-bars', 'seq-bars'
 	for i in df['block_progressive_ID'].unique():
 		block_df = df[df['block_progressive_ID']==i]
 		x, y = plot_figure3(block_df, left_colNames, right_colNames, -100, 101, 20, show=False)
@@ -65,31 +74,36 @@ def plot_figure3_subplots(df, left_colNames, right_colNames, dvmin=-100, dvmax=1
 
 
 # FIGURE FOUR A
-def plot_figure4(df, left_colNames, right_colNames, dvmin=-60, dvmax=61, step=10, show=False):
+def plot_figure4(df, left_colNames, right_colNames, dvmin=-60, dvmax=61, step=10, show=False, write_to=None):
 	"""
-	dataframe, list, list -> None
+	dataframe, list, list, int, int, int, bool, str -> list, list
 	"""
 	left = df[left_colNames]
 	right = df[right_colNames]
 	left.rename(columns=dict(zip(left_colNames, range(1,7))), inplace=True)
 	right.rename(columns=dict(zip(right_colNames, range(1,7))), inplace=True)
 
-	diff = left.combine(right, lambda l, r: l-r) # item-wise left minus right 
+	# item-wise matrix operation, left minus right. positive if left value is greater
+	diff = left.combine(right, lambda l, r: l-r) 
+	# count how many times left >= right
 	diff['count'] = diff.gt(0).sum(axis=1)
-
+	# sum(left) - sum(right)
 	diff['diff'] = left.sum(axis=1)-right.sum(axis=1)
+	# 1 chose left, 0 chose right. Flipped 'side_chosen' column
 	diff['choice'] = np.logical_xor(df['side_chosen'],1).astype(int)
-
 	diff = diff.sort_values(by=['diff'], ascending=True)
 
-	#print('Fig 4A Blue line')
+	# Fig 4A Blue line
+	# calculate prob(choose left) for each bucket when left >= right MORE than three times 
+	# left win times: 6/0, 5/1, 4/2. tie 3/3 ignored
 	wgt3 = diff.loc[diff['count'] > 3]
 	wgt3_res = group(wgt3, dvmin, dvmax, step)
 
-	#print('\nFig 4A Red line')
+	# Fig 4A Red line
+	# calculate prob(choose left) for each bucket when left >= right FEWER than three times 
+	# left win times: 0/6, 1/5, 2/4. tie 3/3 ignored
 	wlt3 = diff.loc[diff['count'] < 3]
 	wlt3_res = group(wlt3, dvmin, dvmax, step)
-	#print(wlt3)
 
 	if show:
 		x = np.array([x for x in range(dvmin, dvmax, step)])
@@ -97,7 +111,8 @@ def plot_figure4(df, left_colNames, right_colNames, dvmin=-60, dvmax=61, step=10
 		fig.add_trace(go.Scatter(x=x, y=wgt3_res, mode='lines', name='W>3', line=dict(color='blue')))
 		fig.add_trace(go.Scatter(x=x, y=wlt3_res, mode='lines', name='W<3', line=dict(color='red')))
 		fig.show()
-
+	if write_to is not None:
+		fig.write_image(write_to)
 	return wgt3_res, wlt3_res
 
 
@@ -105,6 +120,7 @@ def plot_figure4_subplots(df, left_colNames, right_colNames, dvmin=-60, dvmax=61
 	fig = make_subplots(rows=2, cols=3, subplot_titles=('sim-numbers', 'alt-numbers', 'seq-numbers', 'sim-bars', 'alt-bars', 'seq-bars'))
 	x = np.array([x for x in range(dvmin, dvmax, step)])
 	
+	# #call plot_figure4() for each block in the order of 'sim-numbers', 'alt-numbers', 'seq-numbers', 'sim-bars', 'alt-bars', 'seq-bars'
 	for i in df['block_progressive_ID'].unique():
 		block_df = df[df['block_progressive_ID']==i]
 		wgt3_res, wlt3_res = plot_figure4(block_df, left_colNames, right_colNames, -60, 61, 10, show=False)
@@ -120,21 +136,25 @@ def plot_figure4_subplots(df, left_colNames, right_colNames, dvmin=-60, dvmax=61
 
 
 if __name__ == '__main__':
+	#plot_dir = argv[1]
+	#data = argv[2]
 	plot_dir = "/Users/qiheng/Desktop/code/celss/plots/"
 	data = '/Users/qiheng/Desktop/code/celss/Averaging_folder/all_files_averaging/averaging_online_files/1_transform/stacked_file.csv'
-	#df = pd.read_csv('/Users/qiheng/Desktop/code/celss/Averaging_folder/video_averaging/new_video_aver.csv', header=0)
-	df = pd.read_csv(data, header=0)
-	
-	df.stack()
-	df = df.set_index(['participant_ID', 'trial_ID'])
 
+	df = pd.read_csv(data, header=0)	
+	df.stack()
+	df = df.set_index(['participant_ID', 'trial_ID']) #panel data. The indices are participant_ID and trial_ID.
+
+	#list of column names for left and right values
 	left_colNames = ['value_left_'+str(i) for i in range(1,7)]
 	right_colNames = ['value_right_'+str(i) for i in range(1,7)]
 
-	#plot_figure3(df, left_colNames, right_colNames, -100, 101, 20)
+	#toggle the comments to switch b/t analyzing the six trial blocks in aggregate or individually
+	#range from -100 to 101 (not including 101), bucket range 20. So we have 10 buckets. 
+	#plot_figure3(df, left_colNames, right_colNames, -100, 101, 20, show=True, write_to=plot_dir+"fig3.png")
 	plot_figure3_subplots(df, left_colNames, right_colNames, -100, 101, 20, show=True, write_to=plot_dir+"fig3_subplots.png")
-
-	#plot_figure_four(df, left_colNames, right_colNames, -60, 61, 10)
+	#range from -60 to 61 (not including 61), bucket range 10. So we have 12 buckets. 
+	#plot_figure_four(df, left_colNames, right_colNames, -60, 61, 10, show=True, write_to=plot_dir+"fig4.png")
 	plot_figure4_subplots(df, left_colNames, right_colNames, -60, 61, 10, show=True, write_to=plot_dir+"fig4_subplots.png")
 
 
--- transform_data_march.py
+++ transform_data_march.py
@@ -2,15 +2,17 @@ import pandas as pd
 import numpy as np 
 from os import listdir
 from os.path import isfile, join
-import sys
+from sys import argv
 
 
 def step1(df):
+	#change texts/booleans to numbers
 	block_type = {'sim-numbers':1, 'alt-numbers':2, 'seq-numbers':3, 'sim-bars':4, 'alt-bars':5, 'seq-bars':6}
 	treatment = {'up':1, 'down':2}
 	start_position = {'a_on_left':1, 'a_on_right':2, 'b_on_left':3, 'b_on_right':4}
 	side_chosen = {'left':0, 'right':1}
 	choice_correct = {False:0, True:1}
+
 	rules = {'block_type':block_type, 'treatment':treatment, 'start_position':start_position, 
 		'side_chosen':side_chosen, 'choice_correct':choice_correct, 'choice_correct.1':choice_correct}
 	for col, rule in rules.items():
@@ -18,13 +20,17 @@ def step1(df):
 			df.loc[df[col]==k, col] = v
 	return df 
 
+
 def step2(df, ID):
 	df['participant_ID'] = ID
 
 	row_num = df.shape[0]
-	if row_num == 180 or row_num == 300: # 180 or 300
+	if row_num == 180 or row_num == 300: # if 180 or 300 rows
+		# 111..., 222..., 333..., 444..., 555..., 666...
 		df['block_progressive_ID'] = [i for i in range(1,7) for _ in range(int(row_num/6))]
+		# 1,2,3,... until 180/300
 		df['trial_ID'] = [x for x in range(1, row_num+1)]
+		# 1,2,3,... until 30/50, repeat six times
 		df['trial_ID_in_block'] = [x for x in range(1, int(row_num/6 +1))] * 6 
 	else:
 		print("NOT 30 OR 50 ROWS " + str(row_num))
@@ -32,21 +38,24 @@ def step2(df, ID):
 		df['trial_ID'] = 0
 		df['trial_ID_in_block'] = 0 
 
-	df['block_order'] = df['block_type'] % 3 + 1 # based on block_type: 1/4->1 sim; 2/5->2 alt; 3/6->3 seq
+	# sim 1, alt 2, seq 3.  Converted from block_type: 1,4->1; 2,5->2; 3,6->3
+	df['block_order'] = df['block_type'] % 3 + 1 
+	# numbers 1, bars 2
 	df['block_stimuli'] = 1
-	df.loc[df['block_type'] >= 4, 'block_stimuli'] = 2 # OR df.loc[df['???'].str.contains('bar', 'block_stimuli'] = 2
-	
-	df['left-0-right'] = df['start_position'].copy() # based on 'start_position', 1/3->1 left; 2/4->2 right; 0->0
+	df.loc[df['block_type'] >= 4, 'block_stimuli'] = 2
+	# left 1, right 2, 0 is 0. Converted 'start_position' on 0/1/2/3/4 scale to 'left-0-right' on 0/1/2 scale. 1,3->1 left; 2,4->2 right; 0->0
+	df['left-0-right'] = df['start_position'].copy() 
 	df.loc[df['left-0-right'] == 3, 'left-0-right'] = 1
 	df.loc[df['left-0-right'] == 4, 'left-0-right'] = 2
 
-	# start_position - 'a_on_right':2, 'b_on_left':3
+	# Corrected the a/b left/right messup. start_position - 'a_on_right':2, 'b_on_left':3
 	a_columns = ['setup_a'+str(i) for i in range(1,7)]
 	b_columns = ['setup_b'+str(i) for i in range(1,7)]
 	should_swap = df['start_position'].isin([2, 3])
 	df.loc[should_swap, a_columns+b_columns] = df.loc[should_swap, b_columns+a_columns].values
 	df.drop(columns=['choice_correct.1'], inplace=True)
 	
+	#rename columns
 	for i in range(1, 7):
 		df['value_left_'+str(i)] = df['setup_a'+str(i)]
 		df['value_right_'+str(i)] = df['setup_b'+str(i)]
@@ -54,44 +63,43 @@ def step2(df, ID):
 
 
 if __name__ == '__main__':
-	input_path = "/Users/qiheng/Desktop/code/celss/Averaging_folder/all_files_averaging/averaging_online_files/1_transform_copy"
+	# if you prefer typing paths in command line: 
+	# input_path = argv[1]
+	# output_path = argv[2]
+	input_path = "/Users/qiheng/Desktop/code/celss/Averaging_folder/all_files_averaging/averaging_online_files/1_transform_original_data"
 	output_path = "/Users/qiheng/Desktop/code/celss/Averaging_folder/all_files_averaging/averaging_online_files/1_transform"
-	
-	#input_path = "/Users/qiheng/Desktop/code/celss/Averaging_folder/video_averaging"
-	#output_path = "/Users/qiheng/Desktop/code/celss/Averaging_folder/video_averaging"
-	
-	files = [f for f in listdir(input_path) if isfile(join(input_path, f)) and not f.startswith('.')]
-	stacked_df = None
-	ID = 1
-
-	#ref_df = pd.DataFrame({'file_names': files, 'participant_ID': [i+1 for i in range(len(files))]})
-	ref_df = pd.DataFrame(columns=['file_names', 'participant_ID', 'num_rows', 'payment', 'avg_resp_time'])
 
 	cols = ['participant_ID', 'treatment', 'block_progressive_ID', 'trial_ID_in_block', 'left-0-right', 'block_type', 'block_order', 'block_stimuli', 'trial_ID', 'start_position', 'value_left_1', 'value_left_2', 'value_left_3', 'value_left_4', 'value_left_5', 'value_left_6',
 		'value_right_1', 'value_right_2', 'value_right_3', 'value_right_4', 'value_right_5', 'value_right_6', 'total_left', 'total_right', 'waiting_elapsed', 'side_chosen', 'choice_correct', 
 		'bonus_block_type', 'bonus_trial_number_chosen', 'bonus_trial_correct', 'bonus_total']
+
+	# filename-ID dataframe
+	ref_df = pd.DataFrame(columns=['file_names', 'participant_ID', 'num_rows', 'payment', 'avg_resp_time'])
+	# stacked dataframe
+	stacked_df = None 
 	
+	files = [f for f in listdir(input_path) if isfile(join(input_path, f)) and not f.startswith('.')]
+	ID = 1
+
 	for f in files:
 		df = pd.read_csv(join(input_path, f), skip_blank_lines=True, header=0)
-		num_rows = df.shape[0]
-		
-		payment = df.iloc[num_rows-1:]['bonus_total'] #, df.columns.get_loc('bonus_total')]
+		row_num = df.shape[0] #get the number of rows
+		payment = df.iloc[row_num-1:]['bonus_total']
 		resp_time = df[['waiting_elapsed']].mean(axis=0)['waiting_elapsed']
-		ref_df = ref_df.append(pd.DataFrame({'file_names':f, 'participant_ID':ID, 'num_rows':num_rows-7, 'payment':payment, 'avg_resp_time':resp_time}))
+		ref_df = ref_df.append(pd.DataFrame({'file_names':f, 'participant_ID':ID, 'num_rows':row_num-7, 'payment':payment, 'avg_resp_time':resp_time}))
 
-		df = df.iloc[:num_rows-7]
-		df = step1(df)
-		df = step2(df, ID)
-		df = df[cols]
-		#df.to_csv(join(output_path, 'new_video_aver.csv'), index=True)
+		df = df.iloc[:row_num-7] #remove trailing rows that only contain "bonus..." columns. only experiment data left. 
+		df = step1(df) 
+		df = step2(df, ID) 
+		df = df[cols] #rearrange columns in the order of cols variable above
 		df.to_csv(join(output_path, f), index=True)
-		if stacked_df is None:
+		if stacked_df is None: #add each csv to stacked dataframe
 			stacked_df = df
 		else:
 			stacked_df = stacked_df.append(df)
 		ID += 1
 
-	stacked_df = stacked_df[cols]
+	stacked_df = stacked_df[cols] #rearrange column order
 	stacked_df.drop(columns=['bonus_block_type', 'bonus_trial_number_chosen', 'bonus_trial_correct', 'bonus_total'], inplace=True)
 	stacked_df.to_csv(join(output_path, 'stacked_file.csv'), index=False)
 	ref_df.to_csv(join(output_path, 'reference.csv'), index=False)
